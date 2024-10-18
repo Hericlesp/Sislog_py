55e9d37b90430d44cc5e1c884438d7511224d1d5
Hericlesp <hericlespaulo05@gmail.com>
2024-10-18 19:32:07 -0300

    Create login.py

---
+++ login.py
@@ -0,0 +1,70 @@
+#     LOGIN
+
+login = []
+senha = []
+tent = 0
+valid= False
+
+def newlog(a):
+    while valid == False and tent<=3:
+        import os
+        os.system('cls')
+        print('==' * 30)
+        print('     VAMOS LOGAR...  ')
+        print('\n')
+        print(tent)
+        print('==' * 20)
+        print(' '*5,'| LOGIN |')
+        print('==' * 20)
+        logEn=str(input('ACESSE SEU LOGIN:  '))
+        print('==' * 20)
+        senEn=str(input('ACESSE SUA SENHA:  '))
+        print('==' * 20)
+        if login == logEn and senha == senEn:
+            print('==' * 20)
+            print('     ACESSO LIBERADO    ')
+            print('==' * 20)
+            #valid = True
+            break
+        else:
+            tent +=1
+
+    if tent==3:
+        print('==' * 20)
+        print('     ACESSO NEGADO!!    ')
+        print('==' * 20)
+        if login != logEn:
+            print('     LOGIN INCORRETO!!    ')
+            print('==' * 20)
+        if senha != senEn:
+            print('     SENHA INCORRETA!!    ')
+            print('==' * 20)
+
+
+
+import os
+os.system('cls')
+
+print('\n' * 6)
+print('==' * 20)
+print("     PRIMEIRO ACESSO??")
+acess=str(input("   ")).lower()
+print('==' * 20)
+print('\n' * 6)
+if acess == "sim":
+    import os
+    os.system('cls')
+    print('==' * 20)
+    print('     | REGISTRAR |    ')
+    print('==' * 20)
+    login=str(input('DEFINA SEU LOGIN:  '))
+    print('==' * 20)
+    senha=str(input('SENHA:  '))
+    print('==' * 20)
+    newlog()
+else:
+    newlog(acess)
+
+# elif acess==("nao" or "NAO" or "Nao" or "não" or "NÃO" or "Não"):
+#     print('\n' * 6)
+    
